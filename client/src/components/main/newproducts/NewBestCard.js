225b14524e76efaee86c362ade4b9ecfa53d4356
Soso-C <sonny.capisciolti@gmail.com>
2022-06-25 15:30:28 +0200

    add card + productData

--- client/src/components/main/newproducts/NewBestCard.js
+++ client/src/components/main/newproducts/NewBestCard.js
@@ -2,24 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MdAddShoppingCart } from "react-icons/md";
 
-export default function NewBestCard() {
+export default function NewBestCard({ product }) {
   return (
     <div className="bg-white h-[350px] w-full p-4 shadow-sm shadow-gray-300">
       <div className="h-3/4">
-        <img
-          src={
-            "https://media.ldlc.com/r150/ld/products/00/05/96/16/LD0005961662.jpg"
-          }
-          className="mx-auto w-52 h-52 pt-8"
-          alt=""
-        />
+        <img src={product.image} className="mx-auto w-52 h-52 pt-8" alt="" />
       </div>
       <div className="h-1/4 flex flex-col justify-end gap-2">
         <Link to="/" className="text-md font-medium">
-          Home cinema 5.0 son surround
+          {product.name}
         </Link>
         <div className="flex justify-between items-center">
-          <span className="font-bold text-blue-600">1234€</span>
+          <span className="font-bold text-blue-600">{product.price}</span>
           <MdAddShoppingCart className="w-7 h-7 text-orange-400" />
         </div>
       </div>
--- client/src/components/main/newproducts/NewOrBestProducts.js
+++ client/src/components/main/newproducts/NewOrBestProducts.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { MdAddShoppingCart } from "react-icons/md";
+import products from "../../../data/productData";
+import NewBestCard from "./NewBestCard";
 
 export default function NewOrBestProducts({ newOrBest }) {
   return (
@@ -9,86 +9,25 @@ export default function NewOrBestProducts({ newOrBest }) {
         {newOrBest === "new" ? "Nouveautés" : "Meilleures ventes"}
       </span>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-8 lg:pt-16">
-        <div className="bg-white h-[350px] w-full p-4 shadow-sm shadow-gray-300">
-          <div className="h-3/4">
-            <img
-              src={
-                "https://media.ldlc.com/r150/ld/products/00/05/96/16/LD0005961662.jpg"
-              }
-              className="mx-auto w-52 h-52 pt-8"
-              alt=""
-            />
-          </div>
-          <div className="h-1/4 flex flex-col justify-end gap-2">
-            <Link to="/" className="text-md font-medium">
-              Home cinema 5.0 son surround
-            </Link>
-            <div className="flex justify-between items-center">
-              <span className="font-bold text-blue-600">1234€</span>
-              <MdAddShoppingCart className="w-7 h-7 text-orange-400" />
-            </div>
-          </div>
-        </div>
-        <div className="bg-white h-[350px] w-full p-4 shadow-sm shadow-gray-300">
-          <div className="h-3/4">
-            <img
-              src={
-                "https://media.ldlc.com/r150/ld/products/00/05/96/16/LD0005961662.jpg"
-              }
-              className="mx-auto w-52 h-52 pt-8"
-              alt=""
-            />
-          </div>
-          <div className="h-1/4 flex flex-col justify-end gap-2">
-            <Link to="/" className="text-md font-medium">
-              Home cinema 5.0 son surround
-            </Link>
-            <div className="flex justify-between items-center">
-              <span className="font-bold text-blue-600">1234€</span>
-              <MdAddShoppingCart className="w-7 h-7 text-orange-400" />
-            </div>
-          </div>
-        </div>
-        <div className="bg-white h-[350px] w-full p-4 shadow-sm shadow-gray-300">
-          <div className="h-3/4">
-            <img
-              src={
-                "https://media.ldlc.com/r150/ld/products/00/05/96/16/LD0005961662.jpg"
-              }
-              className="mx-auto w-52 h-52 pt-8"
-              alt=""
-            />
-          </div>
-          <div className="h-1/4 flex flex-col justify-end gap-2">
-            <Link to="/" className="text-md font-medium">
-              Home cinema 5.0 son surround
-            </Link>
-            <div className="flex justify-between items-center">
-              <span className="font-bold text-blue-600">1234€</span>
-              <MdAddShoppingCart className="w-7 h-7 text-orange-400" />
-            </div>
-          </div>
-        </div>
-        <div className="bg-white h-[350px] w-full p-4 shadow-sm shadow-gray-300">
-          <div className="h-3/4">
-            <img
-              src={
-                "https://media.ldlc.com/r150/ld/products/00/05/96/16/LD0005961662.jpg"
-              }
-              className="mx-auto w-52 h-52 pt-8"
-              alt=""
-            />
-          </div>
-          <div className="h-1/4 flex flex-col justify-end gap-2">
-            <Link to="/" className="text-md font-medium">
-              Home cinema 5.0 son surround
-            </Link>
-            <div className="flex justify-between items-center">
-              <span className="font-bold text-blue-600">1234€</span>
-              <MdAddShoppingCart className="w-7 h-7 text-orange-400" />
-            </div>
-          </div>
-        </div>
+        {newOrBest === "new"
+          ? products.products
+              .filter(
+                (product) =>
+                  product.newProduct === true && product.bestSelling !== true
+              )
+              .slice(0, 4)
+              .map((product, index) => {
+                return <NewBestCard key={product.id} product={product} />;
+              })
+          : products.products
+              .filter(
+                (product) =>
+                  product.bestSelling === true && product.newProduct !== true
+              )
+              .slice(0, 4)
+              .map((product, index) => {
+                return <NewBestCard key={product.id} product={product} />;
+              })}
       </div>
     </div>
   );
